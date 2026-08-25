4b644b3d18e2e4021217180052bef99cb4c9e4eb
DylanFry <dylan-fry@live.nmit.ac.nz>
2026-08-25 13:52:44 +1200

    Basic game UI layout

--- docs/DEVLOG.md
+++ docs/DEVLOG.md
@@ -1,3 +1,10 @@
+> Commit: Basic game UI layout
+> - The game now has the main interface elements
+> - This includes the user input, a submit button, the output log and the image display.
+> - The image display is able to show multiple images on top of each other and the text output is able to show the backlog of multiple lines of text.
+> - Currently the text output has problems with word-wrapping and I haven't tested if it can scroll on overflow.
+> - It may need to be changed to a different element in the future.
+
 > Commit: Basic classes and test window
 > - Most classes have files and empty classes created
 > - This is likely going to gain or lose some classes as I find out what I need
--- scripts/gui.py
+++ scripts/gui.py
@@ -3,16 +3,28 @@ from input_parser import Parser
 
 class GUI:
 	def __init__(self):
+		self.log = ''
 		self.window = self.create_window()
   
 	def create_window(self):
-		layout = [[sg.Text('Test')]]
-		return sg.Window('Test', layout=layout, size=(800, 600))
+		layout = [
+     						[sg.Push(), sg.Graph(canvas_size=(512, 256), graph_bottom_left=(0,0), graph_top_right=(512, 256), key='-DISPLAY-'), sg.Push()],
+            			[sg.Text('', background_color='#222222', size=(100, 10), key='-OUTPUT-')],
+               		[sg.Input(key='-IN-', size=(100, 1)), sg.Button('Submit', key='-SUBMIT-', size=(6,1))]
+                 	]
+		return sg.Window('Test', layout=layout, size=(800, 480), finalize=True)
 
 	def run_game(self, parser, window):
 		while True:
 			event, values = window.read()
-
+   
 			if event == sg.WIN_CLOSED:
 				break
+
+			window['-DISPLAY-'].draw_image(filename='./assets/Sleeping Quarters.png', location=(0, 256))
+			window['-DISPLAY-'].draw_image(filename='./assets/Underling.png', location=(256-64, 256))	
+ 
+			self.log += '> ' + values['-IN-'] + '\n'
+ 
+			window['-OUTPUT-'].update(self.log)
 		window.close()
